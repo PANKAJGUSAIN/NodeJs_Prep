074bc5de0477370b57ccfddfdc474acca947382f
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-04-18 01:17:38 +0530

    rendering a list of items in report.ejs

--- P9 - TemplateEngineEJS/Routes/Report.js
+++ P9 - TemplateEngineEJS/Routes/Report.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 router.get('/report' , (req , res)=>{
     console.log('report route');
-    res.render('report');
+    const student = [
+        { name: "Pankaj", grade: "10th", favouriteSubject: "Maths" },
+        { name: "Riya", grade: "9th", favouriteSubject: "Science" },
+        { name: "Amit", grade: "11th", favouriteSubject: "History" },
+        { name: "Sneha", grade: "12th", favouriteSubject: "Physics" },
+        { name: "Rahul", grade: "8th", favouriteSubject: "English" }
+    ];
+    res.render('report' , { title:"Report" , student} );
 })
 
 module.exports = router;
