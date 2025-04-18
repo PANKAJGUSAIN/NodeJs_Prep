4a548b72cbda0c3974475ed693e4dedb777e670c
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-04-18 14:21:22 +0530

    Refactor output.txt to remove redundant content and retain essential text for clarity and conciseness.

--- P10 - Streams/app.js
+++ P10 - Streams/app.js
@@ -8,7 +8,16 @@ const readableStream = fs.createReadStream(inputFilePath , {encoding:"utf8" , hi
 const writeableStream = fs.createWriteStream(outputFilePath);
 
 //connect readableStream and writeableStream
-readableStream.pipe(writeableStream);
+
+//way1 
+// readableStream.pipe(writeableStream); 
+
+//way2 
+readableStream.on("data" , (chunk)=>{
+    console.log("Buffer(Chunk)" , Buffer.from(chunk)); // convert the chunk to a buffer
+    console.log("Recieved Chunk" , chunk);  // Logs each 16-byte chunk
+    writeableStream.write(chunk);          // write each chunk to output file
+})
 
 readableStream.on("error" , (err)=> console.log(err));
 writeableStream.on("error" , (err)=> console.log(err));
