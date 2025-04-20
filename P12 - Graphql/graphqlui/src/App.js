259b86d085487024a07fef91467b7828998e1d80
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-04-20 20:55:36 +0530

    add Apollo Client setup and integrate GraphQL queries in App component

--- P12 - Graphql/graphqlui/src/App.js
+++ P12 - Graphql/graphqlui/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { gql, useQuery } from '@apollo/client';
 import './App.css';
 
+
+const query = gql`
+query getAllTodos{
+  getTodos {
+    id,
+    title,
+    user {
+      id,
+      name
+    }
+  }
+}`
+
 function App() {
+  const {data , error ,  loading } =  useQuery(query);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <>Loading...</>
+      ) : error ? (
+        <>Error: {error.message}</>
+      ) : (
+        <table border="1" style={{width:'100%' , height:"200px" , }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>User ID</th>
+              <th>User Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.getTodos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+                <td>{todo.user.id}</td>
+                <td>{todo.user.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
